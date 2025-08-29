6a829dae69bc47eeddfcf1791b986d983d75a584
Ozcon99 <ozcon09@gmail.com>
2025-08-29 08:03:44 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ if c == "0":
     os.system("pip install socks")
     os.system("pip install pysocks")
     os.system("pip install colorama")
-    os.system("pip install undetected_chromedriver")
     os.system("pip install httpx")
     os.system("git pull")
 
@@ -23,7 +22,6 @@ elif c == "1":
     os.system("pip3 install socks")
     os.system("pip3 install pysocks")
     os.system("pip3 install colorama")
-    os.system("pip3 install undetected_chromedriver")
     os.system("pip3 install httpx")
     os.system("git pull")
    
